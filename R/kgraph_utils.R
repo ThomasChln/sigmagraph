54fbcae20808a405f1971dac223233c3f62edc8d
tcharlon <charlon@protonmail.com>
2024-04-08 08:13:03 +0200

    kgraph utils

--- R/kgraph_utils.R
+++ R/kgraph_utils.R
@@ -1,4 +1,64 @@
 
+# attenuate edges, highlight multiple connected nodes
+highlight_multiple_connected = function(df_links, selected_phenos) {
+
+  df_links_pheno = subset(df_links, from %in% selected_phenos)
+
+  if (is.null(names(selected_phenos))) {
+
+    full_connect_nodes = df_links_pheno %$%
+      { names(which(table(to) == length(selected_phenos))) }
+ 
+    mult_connect_nodes = df_links_pheno %$%
+      { names(which(table(to) > 1)) }
+
+  } else {
+
+    df_links_pheno$group = df_links_pheno %$%
+        stringi:::stri_replace_all_fixed(from, selected_phenos, names(selected_phenos), vectorize_all = FALSE)
+    df_group_uniq = unique(df_links_pheno[c('group', 'to')])
+    full_connect_nodes = df_group_uniq %$%
+      { names(which(table(to) == length(unique(group)))) }
+ 
+    mult_connect_nodes = df_group_uniq %$%
+      { names(which(table(to) > 1)) }
+  }
+
+  df_links$color = if (length(mult_connect_nodes) > 20) "#efefef" else "#ddd"
+  mult_highlight = if (length(mult_connect_nodes) > 20) "#bbb" else "#444"
+  full_highlight = if (length(full_connect_nodes) > 20) "#ddd" else "#444"
+
+  df_links$color[df_links$to %in% mult_connect_nodes] = mult_highlight
+  df_links$color[df_links$to %in% full_connect_nodes] = full_highlight
+
+  df_links$zindex = (df_links$to %in% full_connect_nodes) + (df_links$to %in% mult_connect_nodes)
+  df_links
+}
+
+# apply spring weights (revert, best not to add target nodes weak links)
+convert_to_spring_weights = function(df_links, selected_phenos = NULL) {
+  df_links$weight %<>% { max(.) - . + 1}
+  if (length(selected_phenos) <= 1) return(df_links)
+
+  df_links_targets = if (length(selected_phenos) == 2) {
+      as.list(selected_phenos)
+  } else {
+      combn(selected_phenos, 2) |> t()
+  }
+  df_links_targets %<>% as.data.frame %>% setNames(c('from', 'to'))
+
+  df_links_targets$weight = max(df_links$weight)# + diff(range(df_links$weight))
+  df_links %<>% rbind(df_links_targets)
+}
+
+scale_graph = function(weights) {
+  #{ (. - min(.)) / (max(.) - min(.)) } %>% `+`(.5) %>% `*`(10)
+  upper_bound_mult = if (length(weights) > 1e3) 2 else 4
+  lower_bound_const = if (length(weights) > 1e3) 2 else 3
+  scale(weights, center = FALSE) %>% `*`(upper_bound_mult) %>%
+      ifelse(. < lower_bound_const, lower_bound_const, .)
+}
+
 kgraph_to_lgraph = function(l_fit_embeds) {
 
   df_projs = l_fit_embeds$df_projs
